91d65bee509831e5dddfcbb898218e573b03f475
bdameworth <dameworthben@gmail.com>
2023-02-08 18:26:04 -0500

    mobile compat for index

--- scripts/detectmob.js
+++ scripts/detectmob.js
@@ -1,12 +1,20 @@
-function initiate(){
-    if (typeof screen.orientation !== 'undefined' || screen.width < 1500 || navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)) { 
-        swapStyleSheet("mobilestylesheet.css");
-     }
-}
 
  
-function swapStyleSheet(sheet) {
-    document.getElementById("pagestyle").setAttribute("href", sheet);  
-}
 
-window.onload = initiate();
+
+(function(window, document, undefined){
+
+    function swapStyleSheet(sheet) {
+        document.getElementById("pagestyle").setAttribute("href", sheet);  
+    }
+    
+    window.onload = init;
+    
+      function init(){
+        if (/*typeof screen.orientation !== 'undefined' ||*/ navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)) { 
+            swapStyleSheet("scripts/mobilestylesheet.css");
+            console.log("mobile mode!");
+         }
+      }
+    
+    })(window, document, undefined);
