35675febe587d924092fb6b9c0a4029ce4e08c3f
bdameworth <dameworthben@gmail.com>
2023-02-08 19:32:38 -0500

    ersion

--- scripts/detectmob.js
+++ scripts/detectmob.js
@@ -11,7 +11,7 @@
     window.onload = init;
     
       function init(){
-        console.log("version 1.0.0");
+        console.log("version 1.0.1");
         if (/*typeof screen.orientation !== 'undefined' ||*/ navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)) { 
             swapStyleSheet("scripts/mobilestylesheet.css");
             console.log("mobile mode!");
