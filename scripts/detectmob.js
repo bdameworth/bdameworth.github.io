84e2fe906a1c55ca39e2af389e12dd7d0b8e355d
bdameworth <dameworthben@gmail.com>
2024-02-21 01:35:45 -0500

    Santa Delivery Mobile

--- scripts/detectmob.js
+++ scripts/detectmob.js
@@ -14,6 +14,11 @@
         console.log("version 1.0.7");
         if (/*typeof screen.orientation !== 'undefined' ||*/ navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)) { 
             swapStyleSheet("scripts/mobilestylesheet.css");
+            var path = window.location.pathname;
+            var page = path.split("/").pop();
+            if(page === "santadmob"){
+              window.location.replace("https://bdameworth.github.io/santadmob.html")
+            }
             console.log("mobile mode!");
          }else{
             console.log("PC mode!");
