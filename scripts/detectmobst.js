bf90300ace87c7bbcc57c7390a3a7e9d9928ebe5
bdameworth <dameworthben@gmail.com>
2024-08-01 16:30:26 -0400

    smashtown
    dfg

---
+++ scripts/detectmobst.js
@@ -0,0 +1,26 @@
+
+ 
+
+
+(function(window, document, undefined){
+
+    function swapStyleSheet(sheet) {
+        document.getElementById("pagestyle").setAttribute("href", sheet);  
+    }
+    
+    window.onload = init;
+    
+      function init(){
+        //console.log("version 1.0.7");
+        if (/*typeof screen.orientation !== 'undefined' ||*/ navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)) { 
+            //swapStyleSheet("scripts/mobilestylesheet.css");
+            //window.location.href = "https://bdameworth.github.io/santadmob.html";
+            window.location.assign("https://bdameworth.github.io/stmob.html");
+            
+         }else{
+            console.log("PC mode!");
+            window.location.assign("https://bdameworth.github.io/stpc.html");
+         }
+      }
+    
+    })(window, document, undefined);
